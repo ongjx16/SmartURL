de6fb0da28041b7638b70d50ea83d615cd3ed14d
ongjx16 <ongjx16@gmail.com>
2023-03-11 00:18:20 +0800

    frontend styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [inputvalue, setInputValue] = useState("");
+  const [shortened, setShortened] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // prevent default form submission behavior
+    //reset form after submission
+    // setInputValue("");
+    const longUrl = "{" + '"longURL": "' + inputvalue + '" }'
+    console.log(JSON.parse(longUrl));
+    fetch('http://localhost:3000/shorten', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.parse(longUrl),
+    })
+      .then(
+        // response => response.json()
+        response => console.log(response)
+      )
+      // .then(data => setData(data))
+      .catch(error => console.error(error));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        URL Shortener
       </header>
+      <text className="Subtitle">
+        Input your lengthy url and get a shortened version!
+      </text>
+      <form className="Form" onSubmit={handleSubmit}>
+        <input className="InputField"
+          type="text"
+          name="name"
+          value={inputvalue}
+          onChange={(e) => setInputValue(e.target.value)} />
+        <input className="Submit" type="submit" name="Shorten" />
+      </form>
+
     </div>
   );
 }
